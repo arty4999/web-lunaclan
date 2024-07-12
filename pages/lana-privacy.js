800e77431d8c93ad876796aafaa65edb4ce1745b
Shri <shrisharvesh7@gmail.com>
2024-07-13 04:04:11 +0800

    Add privacy policy and terms of service pages for LanaBot

---
+++ pages/lana-privacy.js
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+
+export default function Privacy() {
+    return (
+        <div>
+            <Head>
+                <title>Privacy Policy - LanaBot</title>
+            </Head>
+
+            <main>
+                <h1>
+                    LanaBot Privacy Policy
+                </h1>
+
+                <p>
+                    This Privacy Policy describes how your personal information is collected, used, and shared when you use LanaBot.
+                </p>
+
+                <section>
+                    <h2>1. Personal Information We Collect</h2>
+                    <p>When you interact with LanaBot, we Do NOT collect any user data.</p>
+
+                    <h2>2. How Do We Use Your Personal Information?</h2>
+                    <p>We don't use any personal information for this bot</p>
+
+                    <h2>3. Sharing Your Personal Information</h2>
+                    <p>We do not share your Personal Information with third parties.</p>
+
+                    <h2>4. Data Retention</h2>
+                    <p>When you interact with LanaBot, data is not retained.</p>
+
+                    <h2>5. Changes</h2>
+                    <p>We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.</p>
+
+                    <h2>6. Contact Us</h2>
+                    <p>For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e-mail at arty@lunaclan.com.</p>
+                </section>
+            </main>
+        </div>
+    );
+}
---
+++ pages/lana-terms.js
@@ -0,0 +1,45 @@
+import Head from 'next/head';
+
+export default function Terms() {
+    return (
+        <div>
+            <Head>
+                <title>Terms of Service - LanaBot</title>
+            </Head>
+
+            <main>
+                <h1>
+                    Terms of Service
+                </h1>
+
+                <p>
+                    <strong>Last updated: 01/05/2024</strong>
+                </p>
+
+                <section>
+                    <h2>1. Introduction</h2>
+                    <p>By accessing our bot ("LanaBot"), you are agreeing to be bound by these Terms of Service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this bot.</p>
+
+                    <h2>2. Use License</h2>
+                    <p>Permission is granted to download, copy, modify, and use the LanaBot for any purpose, commercial or non-commercial. This is the grant of a license, not a transfer of title. Under this license:
+
+                        - You may not hold the creator of LanaBot responsible for any damages or losses, direct or indirect, that arise from the use or modification of the bot.
+                        - You may not represent the modified code as being endorsed by the original creator.
+                    </p>
+
+                    <h2>3. Disclaimer</h2>
+                    <p>The materials on LanaBot are provided on an 'as is' basis. LanaBot makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.                    </p>
+
+                    <h2>4. Limitations</h2>
+                    <p>In no event shall LanaBot or its creator be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on LanaBot, even if LanaBot or a LanaBot authorized representative has been notified orally or in writing of the possibility of such damage.</p>
+
+                    <h2>5. Governing Law</h2>
+                    <p>Any claim relating to LanaBot shall be governed by the laws of the owner's home jurisdiction without regard to its conflict of law provisions.</p>
+
+                    <h2>6. Modifications</h2>
+                    <p>LanaBot may revise these Terms of Service for its bot at any time without notice. By using this bot you are agreeing to be bound by the then current version of these Terms of Service.</p>
+                </section>
+            </main>
+        </div>
+    );
+}
